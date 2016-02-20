771fc4c81e3af96993a4e525a3144585ae4c12d4
Aidan Fitzgerald <duckjr159@gmail.com>
2016-02-20 11:28:03 -0500

    Added colors

--- main.c
+++ main.c
@@ -13,31 +13,43 @@ int main() {
   screen s;
   color c;
 
-  // init red, go to blue (thru purple or green???)
+  clear_screen(s);
+
+  // init red, go to blue (thru purple)
   c.red = MAX_COLOR;
   c.green = 0;
   c.blue = 0;
+  
+  const double DEGREE = M_PI / 180;
 
-  double DEGREE = M_PI / 180;
+  int theta;
 
-  // init 0
-  double theta = 0;
-
-  // count to 90
-  while (theta <= M_PI_2) {
+  for (theta = 0; theta <= 90;) {
     int x1 = 0;
-    int y1 = (int) (YRES * cos(theta));
+    int y1 = (int) (YRES * cos(theta*DEGREE));
 
-    int x2 = (int) (XRES * sin(theta));
+    int x2 = (int) (XRES * sin(theta*DEGREE));
     int y2 = 0;
 
     draw_line(x1, y1, x2, y2, s, c);
 
-    theta += DEGREE;
+    // printf("%d %d %d %d \n", theta, c.red, c.green, c.blue);
+
+    // Moved increment statement into loop
+    theta++;
     
+    if (theta <= 45) {
+      c.blue = theta * 255 / 45;
+    }
+    if (theta >= 45) {
+      c.red = (90 - theta) * 255 / 45;
+    }
   }
 
   save_extension(s, "envelope.png");
   display(s);
 
+  // for testing purposes
+  //save_ppm(s, "envelope.ppm");
+  
 }
