f296d566d78bf52ca05ceb505fada42eb1e6a390
Aidan Fitzgerald <duckjr159@gmail.com>
2016-03-16 11:47:21 -0400

    starting parser

--- parser.py
+++ parser.py
@@ -1,7 +1,33 @@
 from display import *
 from matrix import *
 from draw import *
+from math import *
 
 def parse_file( fname, points, transform, screen, color ):
-    pass
-
+    # Setup edges as empty list
+    edges = []
+    T = new_matrix()
+    # Open file, read script, and close it
+    script = None
+    with open(fname) as f:
+        script = f.read()
+    # Parse script - separate at whitespace
+    args = script.split()
+    itr = iter(args)
+    
+    # Do not loop through the list because we need to get multiple elements
+    while True:
+        cmd = next(itr, "quit").lower()
+        # Drawing routines
+        if cmd == "line":
+            add_edge(edges, float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), \
+                     float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)))
+        elif cmd == "circle":
+            add_circle(edges, float(next(itr, 0)), float(next(itr, 0)), 0, float(next(itr, 0)), 0.05)
+        elif cmd == "hermite":
+            add_curve(edges, float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), 0.05, HERMITE)
+        elif cmd == "bezier":
+            add_curve(edges, float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), float(next(itr, 0)), 0.05, BEZIER)
+        # control operations
+        elif cmd == "quit":
+            return
