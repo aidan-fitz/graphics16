1785477715312bcfad8729c2e8d2fdfed2f52cd8
Aidan Fitzgerald <duckjr159@gmail.com>
2016-04-15 11:11:42 -0400

    +stack.py

---
+++ stack.py
@@ -0,0 +1,16 @@
+import matrix
+
+class Stack:
+    def __init__(self):
+        self.stack = []
+        I = new_matrix()
+        ident()
+        self.stack.append(I)
+
+    def push():
+        clone = [row[:] for row in self.stack[-1][:]]
+        self.stack.append(clone)
+
+    def pop():
+        if len(stack) > 1:
+            return self.stack.pop()
